c6c21e33f862628eb5f8fd5d7115ee1d4433668a
= <inzilagiway@gmail.com>
2022-02-08 23:49:03 +0200

    finishing up

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const targetWords = ["woman", "crane", "sugar"]
 const guessGrid = document.querySelector(".custom-grid")
 const targetWord = targetWords[Math.floor(Math.random() * targetWords.length)]
 const alerts = document.querySelector(".alerts")
+const keyboard = document.querySelector(".keyboard")
     // const keys = document.querySelectorAll(".key")
     // const cells = document.querySelectorAll(".cell")
     // const rows = document.querySelectorAll(".row")
@@ -76,8 +77,48 @@ const submit = () => {
     }
 
     const guess = activeCells.reduce((acc, c) => acc + c.dataset.character, "")
+    stopInteraction()
+    activeCells.forEach((cell, i, arr) => flipCell(cell, i, arr, guess))
+}
 
-    // activeCells.forEach
+const flipCell = (cell, i, arr, guess) => {
+    const char = cell.dataset.character
+    const key = keyboard.querySelector(`[data-key=${char}]`)
+    setTimeout(() => {
+        cell.classList.add("flip")
+    }, i * 500 / 2)
+    cell.addEventListener("transitionend", () => {
+        cell.classList.remove("flip")
+        cell.classList.remove("active")
+        if (targetWord[i] === char) {
+            cell.classList.add("correct")
+            key.classList.add("correct")
+        } else if (targetWord.includes(char)) {
+            cell.classList.add("wrong-location")
+            key.classList.add("wrong-location")
+        } else {
+            cell.classList.add("wrong")
+            key.classList.add("wrong")
+        }
+        if (i === arr.length - 1) {
+            cell.addEventListener("transitionend", () => {
+                startInteraction()
+                checkEnd(guess, arr)
+            }, { once: true })
+        }
+    }, { once: true })
+}
+
+const checkEnd = (guess, cells) => {
+    if (guess === targetWord) {
+        stopInteraction()
+        showAlert("You win واو شاطر")
+        return danceCells(cells)
+    }
+    if (guessGrid.querySelectorAll(":not([data-character])").length === 0) {
+        stopInteraction()
+        showAlert("You lose. try again tomorrow", null)
+    }
 }
 
 const showAlert = (text, dur = 1000) => {
@@ -86,13 +127,24 @@ const showAlert = (text, dur = 1000) => {
     alert.classList.add("alert")
     alerts.prepend(alert)
     alert.addEventListener("transitionend", () => alert.remove())
-    setTimeout(() => alert.classList.add("hide"), dur)
+    dur &&
+        setTimeout(() => alert.classList.add("hide"), dur)
 }
 
 const shakeCells = (cells) => {
     cells.forEach(c => {
         c.classList.add("shake")
         c.addEventListener("animationend", () => c.classList.remove("shake"), { once: true })
+    })
+}
+
+const danceCells = (cells) => {
+    cells.forEach((c, i) => {
+        setTimeout(() => {
+
+            c.classList.add("dance")
+            c.addEventListener("animationend", () => c.classList.remove("shake"), { once: true })
+        }, i * 100)
     })
 }
 
